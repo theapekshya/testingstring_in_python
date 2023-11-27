33f824409eded09c22bb91b12742dc53a0100eff
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-27 20:05:09 +0545

    testingstring_in_python

---
+++ string test.py
@@ -0,0 +1,17 @@
+# testing string
+
+s1='HELLO'
+print(s1.isupper())
+print(s1.islower())
+s2='Hello Nepal'
+print(s2.istitle())
+s3='1234'
+a=s3.isdigit()
+print(a)
+s4='name appu'
+b=s4.isalpha()
+c=s4.isalnum()
+print(c)
+print(b)
+s5='   '
+print(s5.isspace())
